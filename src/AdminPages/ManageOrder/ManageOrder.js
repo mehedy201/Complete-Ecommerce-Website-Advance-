e901de580ab4f9094d0ef4ade578e2aabf631852
mehedy201 <mehedy643@gmail.com>
2022-12-08 11:41:01 +0600

    created admin pages

--- src/AdminPages/ManageOrder/ManageOrder.js
+++ src/AdminPages/ManageOrder/ManageOrder.js
@@ -1,9 +1,26 @@
+import { Divider } from 'antd';
 import React from 'react';
+import './ManageOrder.css'
+import laptop from '../../Images/Hp-removebg-preview.png'
+import phone from '../../Images/Iphone14.jpg'
+import headphone from '../../Images/headphones.jpg'
 
 const ManageOrder = () => {
     return (
-        <div>
-            <h1>Manage Order</h1>
+        <div className='container mt-3'>
+            <h4>Manage Order</h4>
+            <Divider className='mt-2'/>
+            <div className='d-flex justify-content-between border mb-1 p-2'>
+                <div className='d-flex'>
+                    <img src={laptop} className='manage_order_image' alt="" />
+                    <h5>Special Laptop</h5>
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <button className='manage_order_button'>Panding</button>
+                    <button className='manage_order_button'>Update Status</button>
+                </div>
+            </div>
+            
         </div>
     );
 };
--- src/AdminPages/ManageProduct/ManageProduct.js
+++ src/AdminPages/ManageProduct/ManageProduct.js
@@ -1,9 +1,24 @@
+import { Divider } from 'antd';
 import React from 'react';
+import './ManageProduct.css'
+import headphone from '../../Images/headphones.jpg'
 
 const ManageProduct = () => {
     return (
-        <div>
-            <h1>Manage Product</h1>
+        <div className='container mt-3'>
+            <h6 className='text-secondary mb-0'>List of Product <span className='text-dark'>100</span></h6>
+            <Divider className='mt-2'/>
+            <div className='d-flex align-items-center justify-content-between p-2 border'>
+                <div className='d-flex align-items-center'>
+                    <img className='manage_product_img' src={headphone} alt="" />
+                    <h5>Special Headphone Title</h5>
+                </div>
+                <div className='d-flex'>
+                    <button className='btn-sm bg-info fw-semibold manage_product_button mx-2'>Preview</button>
+                    <button className='btn-sm bg-light fw-semibold manage_product_button mx-2'>Edit</button>
+                    <button className='btn-sm bg-danger fw-semibold manage_product_button mx-2'>Delete</button>
+                </div>
+            </div>
         </div>
     );
 };

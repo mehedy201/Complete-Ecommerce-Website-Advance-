95fb85a65f098be3e6145906767d8cb11e6abde4
mehedy201 <mehedy643@gmail.com>
2023-03-16 19:20:55 +0600

    completely done home page subscriber components

--- src/Componentes/SubscribeNow/SubscribeNow.js
+++ src/Componentes/SubscribeNow/SubscribeNow.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 const SubscribeNow = () => {
+
+
+    // React Hook Form --------------------------------------
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const onSubmit = data => {
+    
+  // Sent Email Data Server ----------------------
+    fetch('http://localhost:5000/subscriber',{
+      method: 'POST',
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => {
+          if(data.success){
+              toast.success('Now Your are member our site.')
+              reset();
+          }
+          if(data.error){
+            toast.error("Email Not Sent")
+          }
+      })
+  }
+
+
     return (
         <div className='container my-4'>
             <div className="row bg-warning align-items-center rounded py-3 px-2">
@@ -8,9 +37,11 @@ const SubscribeNow = () => {
                     <h5>Subscribe Now</h5>
                     <p className='mb-0'>Get Free email from our site and regular Update!</p>
                 </div>
-                <div className="col-md-6 col-sm-6 d-flex">
-                    <input type="text" className='form-control me-2' />
-                    <button className='btn btn-info'>Subscribe</button>
+                <div className="col-md-6 col-sm-6">
+                    <form className='d-flex' onSubmit={handleSubmit(onSubmit)}>
+                        <input type="email" className='form-control me-2' {...register("email")}/>
+                        <button type='submit' className='btn btn-info'>Subscribe</button>
+                    </form>
                 </div>
             </div>
         </div>
